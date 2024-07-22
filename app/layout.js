c7b60803f86684b0bd59f67f28954be4ecbb61c0
WaiYan MoeMyint <wymoemyint4523@gmail.com>
2024-07-22 14:29:59 +0630

    Update Font

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
-import { Nunito } from "next/font/google";
-const inter = Nunito({ subsets: ["latin"] });
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 import Weather from "./context/Weather";
 
 export const metadata = {
