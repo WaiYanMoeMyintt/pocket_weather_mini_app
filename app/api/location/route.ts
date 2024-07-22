a609e9c0c49ebddb5ac63fc496492d321f2217ca
WaiYan MoeMyint <wymoemyint4523@gmail.com>
2024-07-22 14:25:24 +0630

    Fix BUG

--- app/api/location/route.ts
+++ app/api/location/route.ts
@@ -1,8 +1,5 @@
-import { NextResponse } from "next/server";
-import React, {useState, useEffect, useContext} from "react"
-
-
-export async function GET(){
+import { NextResponse, NextRequest } from "next/server";
+export async function GET(req: NextRequest){
     try {
         const api = `http://api.openweathermap.org/geo/1.0/direct?q=yangon&appid=40957144dd09f20d66c6d541c84a3adf`
         const fetchData = await fetch(api);
